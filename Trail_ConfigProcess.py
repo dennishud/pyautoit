bc1d0307ee76bc4c48cfbcb5cc93ff7e28a67ce7
dennishud <dennishud@outlook.com>
2016-11-19 17:50:32 +0800

    Create Trail_ConfigProcess.py

---
+++ Trail_ConfigProcess.py
@@ -0,0 +1,15 @@
+# coding=utf8
+
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.readfp(open(raw_input("input file name:"), "rb"))
+print config.get("global", "ip")
+
+# config.ini
+#
+# [global]
+# ip = 192.168.1.100
+# ip地址
+# port = 3306
+# password = 123456
